faea55bfac24fe7f41c2032ff71572610297f77e
Yani Asari <yani.iman1@gmail.com>
2020-08-07 06:33:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,7 @@
 # Add  code here!
-def prime?(num)
-  if num > 1 
-  elsif n = 2 
-  elsif num % n == 0 
-  elsif false
-  elsif n += 1 
-    else
-      return true
+def prime?(number)
+  start = 2
+	if number > 1
+		range = (start..number-1).to_a
   end
 end
